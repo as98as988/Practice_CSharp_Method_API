039323e572d0d8ea1b7a7fc09cc0ff0965e0a4a0
as98as988 <57396803+as98as988@users.noreply.github.com>
2019-12-03 20:02:05 +0800

    fox1

---
+++ UnityProject/Assets/fox.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fox : MonoBehaviour
+{
+   
+    [Header("速度"), Range(0, 100f)]
+    public float speed = 10.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float speed = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+
+        transform.Translate(0.1f, speed, 10);
+    }
+    private void Rig()
+    {
+      
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
